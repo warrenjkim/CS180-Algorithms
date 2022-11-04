5de460312f374b8f1d3862d669c55a9c9d41101b
BlankMKB <blankmkb@gmail.com>
2022-11-04 02:12:14 -0700

    refactored driver code, added majority element algorithm

--- algorithms.cpp
+++ algorithms.cpp
@@ -3,20 +3,34 @@
 
 int main()
 {
-  std::cout << "hello world" << std::endl;
-  std::cin.get();
+  std::cout << "merge sort" << std::endl;
 
   int arr[] = {12, 11, 13, 5, 6, 7};
   int size = sizeof(arr) / sizeof(arr[0]);
 
-  
-  srt::mergeSort(arr, 0, size - 1);
+  std::cout << "before sort" << std::endl;
 
-
-  for(int i = 0; i < size; i++) {
+  for(int i = 0; i < size; i++) 
       std::cout << arr[i] << " ";
-  }
 
+  algo::mergeSort(arr, 0, size - 1);
+
+  std::cout << "\nafter sort" << std::endl;
+
+  for(int i = 0; i < size; i++)
+      std::cout << arr[i] << " ";
+
+  int a[] = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+  
+  std::cout << "\n\nmajority element" << std::endl;
+
+  std::cout << "array" << std::endl;
+
+  for(int i = 0; i < (sizeof(a) / sizeof(a[0])); i++)
+    std::cout << a[i] << " ";
+
+  std::cout << "majority element is: " <<  algo::majorityElement(a, sizeof(a) / sizeof(a[0])) << std::endl;
+  
   std::cout << "\ndone!" << std::endl;
   
   return 0;
--- algorithms.hpp
+++ algorithms.hpp
@@ -1,13 +1,13 @@
+// this header file is appropriately names algorithms.hpp
+// this file encapsulates most algorithms covered in CS180 lectures
+// this file uses namespace algo for all algorithms
 #include <vector>
+#include <iostream>
 
-
-// merge sort algorithm
-
-namespace srt {
-  //helper function merge()
-  // merge() merges two halves of the array
-
-  namespace {
+namespace algo {
+  
+  // namespace internal for helper functions not intended for client use
+  namespace internal {
     // helper function merge()
     // returns: void
     // purpose: merge two halves of an array (each side sorted)
@@ -34,10 +34,7 @@ namespace srt {
       // merge the two arrays using two pointer paradigm
       while(left < leftSize && right < rightSize) {
 	// add the smaller of the two values to the array
-	if(leftArr[left] < rightArr[right])
-	  arr[curr++] = leftArr[left++];
-	else
-	  arr[curr++] = rightArr[right++];
+	arr[curr++] = leftArr[left] < rightArr[right] ? leftArr[left++]: rightArr[right++];
       }
 
       // pour the rest of each array into the final array
@@ -51,16 +48,35 @@ namespace srt {
       delete[] leftArr;
       delete[] rightArr;
 
-      // end of function      
+      // end of function merge()
     }
+
+    template<typename K, typename V>
+    struct pair {
+      K first;
+      V second;
+
+      pair() : first(K()), second(V()) { }
+      pair(K first, V second) : first(first), second(second) { }
+    };
+
+    // end of namespace internal
   }
 
+  using namespace internal;
+
+
+  // function mergeSort()
+  // parameters: raw array (int*), start position (int), end position (int)
+  // this algorithm runs in O(nlog(n)) time
+  // Note: this function is written first because it is necessary for a lot of other algorithms
+  
   void mergeSort(int* arr, int start, int end) {
     // base case: if start >= end
     if(start >= end)
       return;
 
-    // recursive case: call mergeSort() on the left and right partitions
+    // recursively call mergeSort() on the left and right partitions
     // calculate midpoint
     int mid = start + (end - start) / 2;
     
@@ -75,5 +91,52 @@ namespace srt {
 
     // end of function
   }
+
+  // function sort()
+  // small wrapper function
+  void sort(int* arr, int size) {
+    mergeSort(arr, 0, size - 1);
+  }
+
+
+
+  // function majorityElement()
+  // parameters: raw array of integers (int*)
+  // this algorithm runs in O(n) time
+  int majorityElement(int* arr, int size) {
+    // auxiliary data structure
+    // first = candidate
+    // second = count
+    pair<int, int> candidate(arr[0], 0);
+
+    // iterate through the array
+    for(int i = 0; i < size; i++) {
+      // if the element is no longer the majority
+      if(candidate.first != arr[i] && candidate.second == 0) {
+	// replace the majority
+	candidate.first = arr[i];
+	candidate.second = 1;
+      }
+      else if(candidate.first == arr[i])
+	candidate.second++;
+      else
+	candidate.second--;
+    }
+
+    // at this point, we have a majority candidate
+    // if the majority has a count of more than n/2, they are the majority, else they're not
+    candidate.second = 0;
+    
+    for(int i = 0; i < size; i++)
+      if(candidate.first == arr[i])
+	candidate.second++;
+
+
+    // return only if the candidate is the majority
+    return candidate.second > size / 2 ? candidate.first : -1;
+    
+    // end of function majorityElement()
+  }
 }
 
+// end of namespace algo
